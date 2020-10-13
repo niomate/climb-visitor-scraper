4154f99d7504afe788608c77cdf86e23521d3b5f
Daniel Gusenburger <danielgusenburger@online.de>
2020-10-13 18:17:34 +0200

    working with mongodb

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use std::{env, error::Error, fmt, fs, io};
-
+use bson::doc;
 use chrono::{DateTime, Utc};
-
-use influxdb::Client;
-use influxdb::InfluxDbWriteable;
-
+use mongodb::Client;
 use scraper::{Html, Selector};
-
-use serde::Deserialize;
-
-use selectors::attr::CaseSensitivity;
+use serde::{Deserialize, Serialize};
+use std::{env, error::Error, fmt, fs, io};
 
 const BASE_URL: &str =
     "https://www.boulderado.de/boulderadoweb/gym-clientcounter/index.php?mode=get&token=";
+const DB: &str = "climbing";
+const COLLECTION: &str = "visitors";
 
-#[derive(Debug, Clone, InfluxDbWriteable)]
+#[derive(Debug, Clone, Serialize)]
 struct VisitorCount {
     time: DateTime<Utc>,
     location: String,
@@ -38,29 +34,54 @@ impl fmt::Display for VisitorCount {
     }
 }
 
+impl VisitorCount {
+    fn from_html(location: &str, doc: &Html) -> VisitorCount {
+        let occ_selector = Selector::parse("div.actcounter-content > span").unwrap();
+        let free_selector = Selector::parse("div.freecounter-content > span").unwrap();
+
+        let occupied = extract_count(&doc, &occ_selector);
+        let free = extract_count(&doc, &free_selector);
+
+        VisitorCount {
+            time: Utc::now(),
+            location: location.to_string(),
+            free,
+            occupied,
+        }
+    }
+}
+
+fn extract_count(doc: &Html, selector: &Selector) -> i32 {
+    match doc.select(selector).next() {
+        Some(elem) => elem.inner_html().parse::<i32>().unwrap(),
+        None => 0,
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Read configuration environment variables
-    let host: String = match env::var("INFLUX_HOST") {
+    let host: String = match env::var("MONGO_HOST") {
         Ok(host) => host,
         Err(_) => "localhost".to_string(),
     };
 
-    let host = format!("http://{}:8086", host);
+    let host = format!("mongodb://{}:27017", host);
 
-    let db: String = match env::var("INFLUX_DB") {
-        Ok(db) => db,
-        Err(_) => "visitors".to_string(),
+    let tokens = match env::var("CLIMBING_TOKENS") {
+        Ok(tokens) => tokens,
+        Err(_) => "tokens.json".to_string(),
     };
-    // Establish connection to InfluxDB
-    let client = Client::new(host, db);
+
+    // Establish connection to MongoDB
+    let client = Client::with_uri_str(&host).await?;
+    let db = client.database(DB);
+    let collection = db.collection(COLLECTION);
 
     // Read boulderado tokens for different climbing gyms
-    let url_file = fs::File::open("tokens.json")?;
+    let url_file = fs::File::open(tokens)?;
     let reader = io::BufReader::new(url_file);
     let urls: Vec<LocationToken> = serde_json::from_reader(reader)?;
-
-    let mut counts: Vec<VisitorCount> = Vec::new();
 
     for LocationToken { location, token } in &urls {
         let url = format!("{}{}", BASE_URL, token);
@@ -68,35 +89,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // Fetch html body from the boulderado webpage
         let body = reqwest::get(&url).await?.text().await?;
         let doc = Html::parse_document(&body);
-        let selector = Selector::parse("div[data-value].zoom").unwrap();
 
-        let mut visitor_count = VisitorCount {
-            time: Utc::now(),
-            location: location.to_owned(),
-            occupied: 0,
-            free: 0,
-        };
+        let visitor_count = VisitorCount::from_html(location, &doc);
 
-        for element in doc.select(&selector) {
-            let deref = element.value();
-            let val = deref.attr("data-value").unwrap().parse::<i32>().unwrap();
-
-            // Depending on what class the tag has, we can deduce whether this 
-            // is the amount of occupied or slots.
-            if deref.has_class("actcounter", CaseSensitivity::CaseSensitive) {
-                visitor_count.occupied = val;
-            } else if deref.has_class("freecounter", CaseSensitivity::CaseSensitive) {
-                visitor_count.free = val;
-            }
-        }
-
-        counts.push(visitor_count.clone());
-    }
-
-    for count in counts {
-        println!("{}", count);
-        let write_res = client.query(&count.into_query("visitors")).await;
-        assert!(write_res.is_ok(), "Error writing to database");
+        println!("{}", visitor_count);
+        let bson_doc = bson::to_document(&visitor_count)?;
+        collection.insert_one(bson_doc, None).await?;
     }
 
     Ok(())
